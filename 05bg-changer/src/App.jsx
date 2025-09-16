fe2fba6ac5bf4181fb5df53979a19a8fd3a28cf0
Barun355 <baruntiwary620@gmail.com>
2025-09-16 15:07:54 +0530

    Background Changer project

---
+++ 05bg-changer/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState("red")
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-end p-10" style={{
+      backgroundColor: backgroundColor
+    }}>
+      <div className="bg-gray-100 w-fit h-fit p-4 rounded-full flex gap-3">
+        <button className="bg-gray-300 rounded-full px-6 py-1">Red</button>
+        <button className="bg-gray-300 rounded-full px-6 py-1">Green</button>
+        <button className="bg-gray-300 rounded-full px-6 py-1">Yellow</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
